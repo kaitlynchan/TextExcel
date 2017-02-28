c7952436c94abab1e728c6ea0be434a132e4747c
Kaitlyn Chan <ununoctium829@gmail.com>
2017-02-28 09:14:57 -0800

    hi?``

---
+++ src/textExcel/TextExcel.java
@@ -0,0 +1,16 @@
+package textExcel;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+// Update this file with your own code. github
+
+public class TextExcel
+{
+
+	public static void main(String[] args)
+	{
+	    System.out.println("HI");
+		// Add your command loop here
+	}
+}
