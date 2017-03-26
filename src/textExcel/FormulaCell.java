fa6c24b8cde458316cf112063321c5d022fbf13f
Kaitlyn Chan <ununoctium829@gmail.com>
2017-03-26 15:21:19 -0700

    B1 done

--- src/textExcel/FormulaCell.java
+++ src/textExcel/FormulaCell.java
@@ -11,7 +11,8 @@ public class FormulaCell extends RealCell implements Cell {
 	
 	@Override
 	public String abbreviatedCellText() {
-		String abrv = (words);
+		
+		String abrv = GetValue(words) + "";
 		abrv += "          ";
 		return abrv.substring(0,10); //pads, then substrings to 10 char
 	}
@@ -20,8 +21,47 @@ public class FormulaCell extends RealCell implements Cell {
 	public String fullCellText() {
 		return (words);
 	}
-	
+	// ( 1 + 3 - 5 )
 	public double GetValue (String words){
-		return Double.parseDouble(words);
+		String [] formulaParts = words.split(" ");
+		String operand1 = "";
+		String operator= "";
+		String operand2 = "";
+		int i = 0;
+		String finalAnswer = formulaParts [i+1];
+		while (i <= formulaParts.length - 5){
+			operand1 = finalAnswer;
+			operator = formulaParts [i+2];
+			operand2 = formulaParts [i+3];
+			finalAnswer = Calculate(operand2, operand1, operator);
+			i+=2;
+		}
+		return Double.parseDouble(finalAnswer);
 	}
+    
+    public static String Calculate(String operand2, String operand1, String operator){
+    	//this method takes in parsed components and performs calculations
+    	String calculateAnswer = " ";
+    	double number2 = Double.parseDouble(operand2);
+    	double number1 = Double.parseDouble(operand1);
+
+    	//calls method depending on operator
+    	if (operator.equals("+")){  		
+    		
+    		calculateAnswer = number1 + number2 + "";
+    	}
+    	else if (operator.equals("-")){
+    
+    		calculateAnswer = number1 - number2 + "";
+    	}
+    	else if (operator.equals("*")){
+    		
+    		calculateAnswer = number1*number2+"";
+    	}
+    	else {
+    		
+    		calculateAnswer = number1/number2+"";
+    	}   
+		return calculateAnswer;
+    }
 }
